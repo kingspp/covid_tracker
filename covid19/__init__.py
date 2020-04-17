f7cc63d0e56a03b4978e185b35486cec492dcd8e
kingspp <kingspprathyush@gmail.com>
2020-04-16 23:34:55 -0400

    Updated list of final countries

--- covid19/__init__.py
+++ covid19/__init__.py
@@ -1,9 +1,11 @@
 import os
-
+import json
 COVID19_MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 from covid19.data import NucleotideData
 from covid19.data import ProteinData
+
+COUNTRIES = json.load(open(os.path.join(COVID19_MODULE_PATH,"data", "COUNTRIES.json")))
 
 
 nucleotide_data = NucleotideData()
