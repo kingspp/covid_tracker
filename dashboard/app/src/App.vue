341f86e398b0a6d04a470d8d3f02661c0dac9ab7
kingspp <kingspprathyush@gmail.com>
2020-04-27 07:08:47 -0400

    Init app

---
+++ dashboard/app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <img alt="Vue logo" src="./assets/logo.png">
+<!--    <HelloWorld msg="Welcome to Your Corona App"/>-->
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+// import { BModal, VBModal, BNavbar } from 'bootstrap-vue'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
