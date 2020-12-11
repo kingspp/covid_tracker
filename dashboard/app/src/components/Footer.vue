f932f56a38b906cd58477be4e0fabf5c4f372c77
kingspp <kingspprathyush@gmail.com>
2020-12-10 21:48:39 -0500

    Fix issues

--- dashboard/app/src/components/Footer.vue
+++ dashboard/app/src/components/Footer.vue
@@ -4,11 +4,11 @@
         <!-- Copyright -->
         <div class="footer-copyright text-center py-3" style="font-weight: bold">Made with <span  style="color: crimson"><font-awesome-icon icon="heart"/></span> by
             <a href="https://kingspp.github.io/" style="color: #FFFFFF!important;">Prathyush</a>,
-            <a href="https://shreeshan.github.io/" style="color: #FFFFFF!important;">Shreesha</a>,
-            <a href="https://www.linkedin.com/in/varun-eranki-046642156/" style="color: #FFFFFF!important;">Varun</a> &
-            <a href="https://www.linkedin.com/in/priyankaphadnis/" style="color: #FFFFFF!important;">Priyanka</a>
-            <a href="https://shreeshan.github.io/" style="color: #FFFFFF!important;">Kevin</a>,
-            <a href="https://www.linkedin.com/in/varun-eranki-046642156/" style="color: #FFFFFF!important;">Ivy</a>
+<!--            <a href="https://shreeshan.github.io/" style="color: #FFFFFF!important;">Shreesha</a>,-->
+<!--            <a href="https://www.linkedin.com/in/varun-eranki-046642156/" style="color: #FFFFFF!important;">Varun</a> &-->
+<!--            <a href="https://www.linkedin.com/in/priyankaphadnis/" style="color: #FFFFFF!important;">Priyanka</a>-->
+            <a href="" style="color: #FFFFFF!important;">Kevin</a>,
+            <a href="" style="color: #FFFFFF!important;">Ivy</a>
 <!--            &-->
 <!--            <a href="https://www.linkedin.com/in/priyankaphadnis/" style="color: #FFFFFF!important;">Priyanka</a>-->
         </div>
